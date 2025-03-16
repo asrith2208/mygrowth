2ab82ade98efc8fb8eba16d72f84a577f8686c85
asrith2208 <uudoonasrith@gmail.com>
2025-03-17 00:17:52 +0530

    Add Python operators example

---
+++ operators.py
@@ -0,0 +1,32 @@
+# Arithmetic Operators
+a = 10
+b = 3
+print(f"Addition:{a + b}")
+print(f"Subtraction:{a - b}")
+print(f"Multiplication:{a * b}")
+print(f"Division:{a / b}")
+print(f"Modulus:{a % b}")
+print(f"Exponentiation:{a ** b}")
+print(f"Floor Division:{a // b}")
+# Comparison Operators
+print(f"{a} > {b}: {a > b}")
+print(f"{a} < {b}: {a < b}")
+print(f"{a} == {b}: {a == b}")
+print(f"{a} != {b}: {a != b}")
+# Logical Operators
+x = True
+y = False
+print(f"x and y: {x and y}")
+print(f"x or y: {x or y}")
+print(f"not y: {not y}")
+# Assignment Operators
+a += 5 # Same as a = a + 5
+print(f"After += : {a}")
+# Bitwise Operators
+
+print(f"a & b: {a & b}") # AND
+print(f"a | b: {a | b}") # OR
+print(f"a ^ b: {a ^ b}") # XOR
+print(f"~a: {~a}") # NOT
+print(f"a << b: {a << 1}") # Left shift
+print(f"a >> b: {a >> b}") # Right shift
