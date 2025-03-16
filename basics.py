c90b16002431baf34edfdff90fc0629b1a5069da
asrith2208 <uudoonasrith@gmail.com>
2025-03-16 23:51:41 +0530

    Add Python basics - variables and data types

---
+++ basics.py
@@ -0,0 +1,19 @@
+# String
+name = "Asrith"
+# Integer
+age = 18
+# Float
+height = 5.9
+# Boolean
+is_coding = True
+# List
+hobbies = ["coding","reading","gaming"]
+# Tuples (Immutable)
+coordinates = (10.5,5.2)
+# Dictioanry
+person = {"name":"Asrith","age":21,"is_coding":True}
+# Output
+print(f"Name:{name}, Age:{age}, Height:{height}")
+print(f"Coding:{is_coding},Hobbies:{hobbies}")
+print(f"Coordinates:{coordinates}")
+print(f"Person Details:{person}")
