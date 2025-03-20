862ca8f4d6b5ae02117b3c7b7a52de60762b9e4a
asrith2208 <uudoonasrith@gmail.com>
2025-03-20 15:54:57 +0530

    Add Conditional statements basics

---
+++ conditional_statements.py
@@ -0,0 +1,26 @@
+# if statement
+x = 10
+if x > 5:
+    print("x is greater than 5")
+
+# If-Else Statement
+x = 3
+if x > 5:
+    print("x is greater than 5")
+else:
+    print("x is not greater than 5")
+
+#  If-Elif-Else Statement
+x = 5
+if x > 5:
+    print("x is greater than 5")
+elif x == 5:
+    print("x is equal to 5")
+else:
+    print("x is less than 5")
+
+# Nested If Statement
+x = 10
+if x > 5:
+    if x < 15:
+        print("x is between 5 and 15")
